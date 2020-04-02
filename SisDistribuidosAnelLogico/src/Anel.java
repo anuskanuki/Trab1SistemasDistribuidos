4899e640f6c0d28ae647433c7b4212b580173f1f
JefersonBonecher <53982003+jefersonbonecher@users.noreply.github.com>
2020-04-02 02:50:55 -0300

    conclusãoClasseAnel

--- SisDistribuidosAnelLogico/src/Anel.java
+++ SisDistribuidosAnelLogico/src/Anel.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Anel {
 
@@ -26,12 +27,12 @@ public class Anel {
                         } else {
                             processosAtivos.add(new Processo(processosAtivos.get(processosAtivos.size() - 1).getPid() + 1, false));
                         }
-                        System.out.println("Processo " + processosAtivos.get(processosAtivos.size() - 1).getPid() + 1);
+                        System.out.println("Processo " + processosAtivos.get(processosAtivos.size() - 1).getPid() + " Criado com sucesso ");
                     }
                     try {
                         Thread.sleep(ADICIONA);
                     } catch (Exception e) {
-                        e.printStackTrace();
+                        
                     }
                 }
             }
@@ -39,14 +40,77 @@ public class Anel {
     }
 
     public void fazRequisicoes() {
-
-    }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+           try {
+                Thread.sleep(REQUISICAO);
+           } catch (Exception e){
+           }
+           synchronized (lock){
+                    if (!processosAtivos.isEmpty()) {
+                        int index = new Random().nextInt (processosAtivos.size());
+                        Processo p = processosAtivos.get(index);
+                        p.enviarRequisicao();
+                        System.out.println("Processo " + p.getPid() + " realizou requisição");
+                    }
+                }
+            }
+        }
+    }).start();
+}
 
     public void inativaProcesso() {
+        new Thread (new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(INATIVO_PROCESSO);
+                    } catch (Exception e) {
+                    }
+                    synchronized (lock) {
+                        if (!processosAtivos.isEmpty()) {
+                            int index = new Random().nextInt(processosAtivos.size());
+                            Processo p = processosAtivos.get(index);
+                            if (p != null && !p.isEhCoordenador()) {
+                                processosAtivos.remove(p);
+                                System.out.println("Processo "+ p.getPid() + " esta inativo");
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
 
     }
 
     public void inativaCoordenador() {
+        new Thread (new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        Thread.sleep(INATIVO_COORDENADOR);
+                    } catch (Exception e) {
+                    }
+                    synchronized (lock){
+                        Processo coordenador = null;
+                        for (Processo p : processosAtivos) {
+                            if (p.isEhCoordenador()) {
+                                coordenador = p;
+                            }
+                        }
+                        if (coordenador != null) {
+                            processosAtivos.remove(coordenador);
+                            System.out.println("Coordenador " + coordenador.getPid() + " esta inativo");
+                            
+                        }
+                    }
+                }
+            }
+        }).start();
 
     }
 
