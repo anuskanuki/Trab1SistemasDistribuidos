b27d324437e5cc970f2c2376d79901eefb1c9209
Anuska <48720231+anuskanuki@users.noreply.github.com>
2020-04-05 20:41:38 -0300

    Add files via upload

---
+++ SisDistribuidosAnelLogico/src/AnelLogico.java
@@ -0,0 +1,104 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AnelLogico {
+
+    private final int processoNovoTimer = 3000;
+    private final int eleicaoNovaTimer = 2500;
+    private final int inativaCoordenadorTimer = 10000;
+    private final int inativaProcessoTimer = 8000;
+
+    public static ArrayList<Processo> processosAtivos;
+    private final Object lockToThread = new Object();
+
+    public AnelLogico() {
+        processosAtivos = new ArrayList<Processo>();
+    }
+
+    public void novoProcesso() {
+        new Thread(() -> {
+            while (true) {
+                synchronized (lockToThread) {
+                    if (verificaProcessos()) {
+                        processosAtivos.add(new Processo(1, true));
+                    } else {
+                        processosAtivos.add(new Processo(processosAtivos.get(processosAtivos.size() - 1).getIdProcesso() + 1, false));
+                    }
+                    System.out.println("Processo criado: " + processosAtivos.get(processosAtivos.size() - 1).getIdProcesso());
+                }
+                try {
+                    Thread.sleep(processoNovoTimer);
+                } catch (InterruptedException e) {
+                }
+            }
+        }).start();
+    }
+
+    public void realizaRequisicao() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(eleicaoNovaTimer);
+                } catch (InterruptedException e) {
+                }
+                synchronized (lockToThread) {
+                    if (!verificaProcessos()) {
+                        int index = new Random().nextInt(processosAtivos.size());
+                        Processo processoRequisicao = processosAtivos.get(index);
+                        processoRequisicao.novaRequisicao();
+                        System.out.println("Processo que fez requisição: " + processoRequisicao.getIdProcesso());
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private boolean verificaProcessos() {
+        return processosAtivos.isEmpty();
+    }
+
+    public void inativarProcesso() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(inativaProcessoTimer);
+                } catch (InterruptedException e) {
+                }
+                synchronized (lockToThread) {
+                    if (!verificaProcessos()) {
+                        int index = new Random().nextInt(processosAtivos.size());
+                        Processo remocao = processosAtivos.get(index);
+                        if (remocao != null && !remocao.isCoordenadorAtual()) {
+                            processosAtivos.remove(remocao);
+                            System.out.println("Processo inativado: " + remocao.getIdProcesso());
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void inativarCoordenador() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(inativaCoordenadorTimer);
+                } catch (InterruptedException e) {
+                }
+                synchronized (lockToThread) {
+                    Processo coordenador = null;
+                    for (Processo processo : processosAtivos) {
+                        if (processo.isCoordenadorAtual()) {
+                            coordenador = processo;
+                        }
+                    }
+                    if (coordenador != null) {
+                        processosAtivos.remove(coordenador);
+                        System.out.println("Coordenador inativado:  " + coordenador.getIdProcesso());
+                    }
+                }
+            }
+        }).start();
+    }
+}
--- SisDistribuidosAnelLogico/src/Processo.java
+++ SisDistribuidosAnelLogico/src/Processo.java
@@ -3,91 +3,89 @@ import java.util.LinkedList;
 
 public class Processo {
 
-    private int pid;
-    private boolean ehCoordenador;
+    private int idProcesso;
+    private boolean coordenadorAtual;
 
-    public Processo(int pid) {
-        setPid(pid);
-    }
-
-    public Processo(int pid, boolean ehCoordenador) {
-        setPid(pid);
-        setEhCoordenador(ehCoordenador);
+    public Processo(int idProcesso) {
+        setIdProcesso(idProcesso);
     }
 
-    public boolean isEhCoordenador() {
-        return ehCoordenador;
-    }
-    public void setEhCoordenador(boolean ehCoordenador) {
-        this.ehCoordenador = ehCoordenador;
+    public Processo(int idProcesso, boolean ehCoordenador) {
+        setIdProcesso(idProcesso);
+        setCoordenadorAtual(ehCoordenador);
     }
 
-    public int getPid() {
-        return pid;
-    }
-    public void setPid(int pid) {
-        this.pid = pid;
+    public boolean isCoordenadorAtual() {
+        return coordenadorAtual;
     }
 
     public boolean novaRequisicao() {
         boolean resultadoDeRequisicao = false;
-        for (Processo p : AnelLogico.ativos) {
-            if (p.isEhCoordenador()) {
-                resultadoDeRequisicao = p.recebeRequisicao(this.pid);
+        for (Processo processo : AnelLogico.processosAtivos) {
+            if (processo.isCoordenadorAtual()) {
+                resultadoDeRequisicao = processo.recebeRequisicao(this.idProcesso);
             }
         }
-        
-        //Se n�o haver coordenador, nova elei��o
         if (!resultadoDeRequisicao) {
             this.novaEleicao();
         }
-        System.out.println("Requisição finalizada.");
+        System.out.println("Requisição finalizada");
         return resultadoDeRequisicao;
     }
 
     private boolean recebeRequisicao(int pidOrigemRequisicao) {
-        System.out.println("Requisição do processo " + pidOrigemRequisicao + " recebida com sucesso.");
+        System.out.println("Requisição do processo " + pidOrigemRequisicao);
         return true;
     }
 
     private void novaEleicao() {
-        System.out.println("Eleição iniciada.");
-        //consulta cada processo, adicionando o id de cada um em uma nova lista
+        System.out.println("-------------------- Eleição iniciada --------------------");
 
         LinkedList<Integer> idProcessosVerificados = new LinkedList<>();
-        for (Processo p : AnelLogico.ativos) {
-            p.consultarProcesso(idProcessosVerificados);
-        }
 
-        //Percorre a lista em busca do maior ID
-        int idNovoCoordenador = this.getPid();
+        AnelLogico.processosAtivos.forEach((p) -> {
+            p.consultarProcesso(idProcessosVerificados);
+        });
+
+        int idNovoCoordenador = this.getIdProcesso();
+
         for (Integer id : idProcessosVerificados) {
             if (id > idNovoCoordenador) {
                 idNovoCoordenador = id;
             }
         }
-        
-        
+
         boolean resultadoAtualizacao = false;
         resultadoAtualizacao = atualizarCoordenador(idNovoCoordenador);
 
         if (resultadoAtualizacao) {
-            System.out.println("Eleiçã finalizada. Novo coordenador é: " + idNovoCoordenador + ".");
+            System.out.println("Eleição FINALIZADA - Novo coordenador: " + idNovoCoordenador);
         } else {
-            System.out.println("Eleição finalizada. Sem novo coordenador.");
+            System.out.println("Eleição FINALIZADA - Sem novo coordenador");
         }
     }
 
     private void consultarProcesso(LinkedList<Integer> processosConsultados) {
-        processosConsultados.add(this.getPid());
+        processosConsultados.add(this.getIdProcesso());
     }
 
     private boolean atualizarCoordenador(int idNovoCoordenador) {
-        
-    	//Atualiza a propriedade EhCoordenador apenas para o novo eleito
-        for (Processo p : AnelLogico.ativos) {
-        	p.setEhCoordenador(p.getPid() == idNovoCoordenador);
-        }
+        AnelLogico.processosAtivos.forEach((processo) -> {
+            processo.setCoordenadorAtual(processo.getIdProcesso() == idNovoCoordenador);
+        });
         return true;
+    }
+
+    //Getters e Setters:
+    public int getIdProcesso() {
+        return idProcesso;
+    }
+
+    public void setIdProcesso(int idProcesso) {
+        this.idProcesso = idProcesso;
+    }
+
+    public void setCoordenadorAtual(boolean coordenadorAtual) {
+        this.coordenadorAtual = coordenadorAtual;
     }
 }
